a414e45e83accdf323f8fdae6421e2812c92cbd7
tirthhshah <tirthhshah18mvclouds@gmail.com>
2026-02-19 16:22:59 +0530

    Rename class and clean up comments in p5.java
    Renamed class from HollowPyramidFiveBase to p5 and removed comments.

--- Day-8/p5.java
+++ Day-8/p5.java
@@ -1,4 +1,4 @@
-public class HollowPyramidFiveBase {
+public class p5 {
     public static void main(String[] args) {
 
         int rows = 5;
@@ -6,18 +6,16 @@ public class HollowPyramidFiveBase {
 
         for (int i = 1; i <= rows; i++) {
 
-            // spacing for center alignment
             for (int j = 1; j <= rows - i; j++) {
                 System.out.print(" ");
             }
 
             if (i == rows) {
-                // print only 5 stars on last row
                 for (int j = 1; j <= baseStars; j++) {
                     System.out.print("* ");
                 }
             } else {
-                // print hollow sides
+             
                 for (int k = 1; k <= 2*i - 1; k++) {
                     if (k == 1 || k == 2*i - 1)
                         System.out.print("*");
